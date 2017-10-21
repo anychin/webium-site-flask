3d77b245e8d0dae5ab58b22b102d09f85d1b04ee
Anton Anuchin <anychin@gmail.com>
2017-10-21 17:03:53 +0300

    add error page 404 and more styles of view welcome page

--- app/app.py
+++ app/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_assets import Environment, Bundle
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('welcome/welcome.html')
+
+@app.route('/ad-context')
+def show_landing_adcontext():
+    return render_template('landing/ad-context.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 # compile assets
 assets = Environment(app)
@@ -15,4 +24,4 @@ css_bundle = Bundle('css/application.sass', filters='sass', output='all.css')
 assets.register('css_all', css_bundle)
 
 if __name__ == "__main__":
-    app.run()    
+    app.run()
